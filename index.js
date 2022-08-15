52c159ae10f038bccada7d2df23dc4db007bb172
Faith Makinde <faithmakinde0@gmail.com>
2022-08-15 04:20:43 +0100

    bug fixed

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
 
-let weather = {
-    paris: {
-      temp: 19.7,
-      humidity: 80
-    },
-    tokyo: {
-      temp: 17.3,
-      humidity: 50
-    },
-    lisbon: {
-      temp: 30.2,
-      humidity: 20
-    },
-    "san francisco": {
-      temp: 20.9,
-      humidity: 100
-    },
-    oslo: {
-      temp: -5,
-      humidity: 20
-    }
-  };
-function foreCast(){
-    let city = prompt("Enter a city");
-    let location = city.toLowerCase();
-    if(weather[location] !== undefined){
-        let temperature = Math.round(weather[location].temp);
-        let humidity = weather[location].humidity;
-        let celFah = Math.round(temperature * (9/5) + 32);
-        alert(`It is currently ${temperature}℃(${celFah}℉) in ${location} with a humidity of ${humidity}`)
-        }
-      else if (weather !== location){
-        let location = city.toLowerCase();
-     alert(
-      `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${location}`);
-     }
-}
-foreCast();
-
-
 function formatDate(now) {
   let hours = now.getHours();
   if (hours < 10) {
