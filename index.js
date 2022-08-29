98cc33944acbe55f9fcab3f107e5f1b2d45f7742
Faith Makinde <faithmakinde0@gmail.com>
2022-08-29 23:25:28 +0100

    implemented another apikey

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function displayForecast(response){
 
   function getForecast(coordinates){
     console.log(coordinates)
-    let apiKey = "c66949765e3b0e53a28c1770749ecb89";
+    let apiKey = "b40b135798f82a05aed08769f9275f50";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast)
   }
@@ -106,7 +106,7 @@ function showTemp(response){
 
 
 function searchCity(city) {
-  let apiKey = "c66949765e3b0e53a28c1770749ecb89";
+  let apiKey = "b40b135798f82a05aed08769f9275f50";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
 }
@@ -131,7 +131,7 @@ function showPosition(position){
   console.log(position)
   let lat = position.coords.latitude;
   let lon =  position.coords.longitude;
-  let apiKey = "c66949765e3b0e53a28c1770749ecb89";
+  let apiKey = "b40b135798f82a05aed08769f9275f50";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
   axios.get(`${url}`).then(showTemp);
